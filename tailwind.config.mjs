eb57ed8f8448fd18c973e0cc9485abbce4fffd23
lds56 <chenrui.qingqing@gmail.com>
2024-04-11 15:02:40 +0800

    add: astro and tailwindcss config

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+	daisyUI: {
+		themes: ["light", "dark", "cupcake"], // 选择要启用的主题
+		// base: true, // 是否使用 daisyUI 的基础样式
+		// utils: true, // 是否包含 daisyUI 的实用程序类
+		// logs: true, // 是否在控制台输出日志
+		// rtl: false, // 是否支持从右到左的布局
+		// prefix: "", // daisyUI 组件的 CSS 前缀
+		// darkTheme: "dark", // 深色模式下使用的主题名称
+	  },
+}
